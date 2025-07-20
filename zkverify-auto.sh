96a54b99861199408f134553da489cf3ff77f948
toanbm85 <94586777+ToanBm@users.noreply.github.com>
2025-07-20 16:26:36 +0700

    Update zkverify-auto.sh

--- zkverify-auto.sh
+++ zkverify-auto.sh
@@ -39,6 +39,15 @@ else
   echo "✅ Circom 2.x is already installed."
 fi
 
+# ========== STEP 0.2: CHECK & INSTALL SNARKJS ==========
+echo "🔎 Checking for snarkjs..."
+if ! command -v snarkjs &> /dev/null; then
+  echo "⚙️  snarkjs not found. Installing snarkjs globally..."
+  npm install -g snarkjs
+else
+  echo "✅ snarkjs is already installed."
+fi
+
 # ========== STEP 1: ENV SETUP ==========
 echo "✅ Installing dependencies..."
 sudo apt update && sudo apt install -y curl git jq
@@ -140,7 +149,7 @@ for i in $(seq 1 $N); do
 
     if [[ $RESPONSE == *"Too Many Requests"* ]]; then
       echo "⏳ Too Many Requests. Waiting before retry..."
-      RETRY_DELAY=$(( RANDOM % 101 + 100 )) # Wait 100-200s
+      RETRY_DELAY=$(( RANDOM % 61 + 60 )) # Wait 60-120s
       echo "⏳ Waiting $RETRY_DELAY seconds before retry..."
       sleep $RETRY_DELAY
       ((ATTEMPT++))
@@ -149,7 +158,7 @@ for i in $(seq 1 $N); do
     fi
   done
 
-  DELAY=$(( RANDOM % 181 + 120 )) # Wait randomly from 120 to 300 seconds (2–5 minutes)
+  DELAY=$(( RANDOM % 61 + 60 )) # Wait randomly from 60 to 120s
   echo "⏳ Waiting $DELAY seconds before next submission... "
   sleep $DELAY
 done
